af57b48ad176b70b3d7b7510aee41ef603ae7491
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2026-01-01 14:08:30 +0700

    Update Quick-Clean.ps1

--- System/Quick-Clean.ps1
+++ System/Quick-Clean.ps1
@@ -1,22 +1,50 @@
-﻿# Quick-Clean.ps1 (v0.1) - A very basic option.
-Write-Host "The script for cleaning temporary files is running." -ForegroundColor Green
+# Quick-Clean.ps1 (v1) - A very basic option.
 
-# 1. Defining the target folder
-$targetFolder = $env:TEMP
+Clear-Host
+Write-Host "======== Cleaning management menu. ========" -ForegroundColor Cyan
+Write-Host "1. Deleting the trash."
+Write-Host "0. Exit"
+Write-Host "===========================================" -ForegroundColor Cyan
 
-# 2. We show you what's in it.
-Write-Host "Folder Contents $($targetFolder):" -ForegroundColor Yellow
-Get-ChildItem -Path $targetFolder
+$choice = Read-Host "Select an option (0-1)"
 
-# 3. Request confirmation from the user
-$userAnswer = Read-Host "Do you really want to delete these files? (Yes/No)"
+switch ($choice) {
+    "0" {
+        Write-Host "Exit..." -ForegroundColor Red
+        exit
+    }
+    "1" {
+        Write-Host "Basket analysis..." -ForegroundColor Yellow
 
-if ($userAnswer -eq 'Yes') {
-    # 4. Deleting it!
-    Remove-Item -Path "$($targetFolder)\*" -Force -Recurse -ErrorAction SilentlyContinue
-    Write-Host "The folder has been cleared!" -ForegroundColor Green
-}  else {
-    Write-Host "The operation was canceled by the user." -ForegroundColor Red
+        $RecycleBin = New-Object -ComObject Shell.Application # Get the size of the current user's shopping cart (in bytes)
+        $RecycleBinItems = $RecycleBin.NameSpace(10).Items() # Please note that NameSpace(10) corresponds to the Recycle Bin folder
+        $TotalSizeBefore = 0
+        foreach ($item in $RecycleBinItems) {
+            $TotalSizeBefore += $item.Size
+        }
+
+        function Format-FileSize {
+            param([long]$Size)
+            if ($Size -ge 1GB) { return "{0:N2} GB" -f ($Size / 1GB) }
+            elseif ($Size -ge 1MB) { return "{0:N2} MB" -f ($Size / 1MB) }
+            elseif ($Size -ge 1KB) { return "{0:N2} KB" -f ($Size / 1KB) }
+            else { return "$Size Bytes" }
+        } # Format the size for easy reading
+
+        $FormattedSizeBefore = Format-FileSize -Size $TotalSizeBefore
+
+        if ($TotalSizeBefore -gt 0) {
+            Write-Host "Found data in the bucket: $FormattedSizeBefore" -ForegroundColor Yellow
+
+            Clear-RecycleBin -Force -ErrorAction SilentlyContinue # Emptying the basket
+            Write-Host "The basket is cleared. Released: $FormattedSizeBefore" -ForegroundColor Green
+        } else {
+            Write-Host "The basket is already empty." -ForegroundColor Gray
+        }
+         Start-Sleep -Seconds 2
+    }
+    Default {
+        Write-Host "Wrong choice!" -ForegroundColor Red
+        Start-Sleep -Seconds 1
+    }
 }
-
-
